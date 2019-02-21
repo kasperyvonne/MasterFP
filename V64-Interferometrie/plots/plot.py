c17d1d7331ec9835c8267a30d22c3be794c30892
Robubabu <robert.appel@udo.edu>
2019-02-21 18:17:20 +0100

    Zehntausenste Korrektur Interferometrie

--- V64-Interferometrie/plots/plot.py
+++ V64-Interferometrie/plots/plot.py
@@ -10,7 +10,7 @@ def mittel(x):              #the real mean()-ing of life
     return ufloat(np.mean(x),np.std(x,ddof=1)/np.sqrt(len(x)))
 def relf(l,m):  #in Prozent
     return (np.absolute(l-m)/l)*100
-lambdvac = 632.990*10**-9 #nano meter
+lambdvac = 632.990*10**(-9) #nano meter
 lengL = ufloat(100,0.1)*10**(-3) #milli meter
 T = 22.8 #celsius
 T+= const.zero_Celsius #kelvin
@@ -20,11 +20,11 @@ kw, kmax , kmin = np.genfromtxt('Kontrast.txt',unpack = True) # grad , mV , V
 k = (kmax[::] - kmin[::])/(kmax[::] + kmin[::])
 umrechni = 2*np.pi/360
 def fitf(x,a,b,c):
-	return np.absolute(a*np.sin(b*x*umrechni)+ c)
+	return np.absolute(a*np.sin(2*x*umrechni + b)+ c)
 #
 #Fit
 np.savetxt('kontrasttab.txt',np.column_stack([kw,noms(k)]), delimiter=' & ',newline= r'\\'+'\n' )
-params , cov = curve_fit(fitf , kw ,k)
+params , cov = curve_fit(fitf , kw ,k, [0.6 , 0.1 , 0])
 params = correlated_values(params, cov)
 for p in params:
     print(p)
@@ -44,7 +44,7 @@ def plaetbrech(tet, a):
 	return a*tet #**2 
 aes = []
 ces = [c1,c2,c3,c4,c5,c6]
-
+wd*= 2*np.pi/360		#umrechnen in Bogenmaß
 for c in ces: 
 	params , cov = curve_fit(plaetbrech , wd ,c)
 	params = correlated_values(params, cov)
@@ -58,14 +58,14 @@ plt.plot(wd, c3,'v',alpha = 0.6 ,label='Messreihe 3')
 plt.plot(wd, c4,'^',alpha = 0.6 ,label='Messreihe 4')
 plt.plot(wd, c5,'<',alpha = 0.6 ,label='Messreihe 5')
 plt.plot(wd, c6,'>',alpha = 0.6 ,label='Messreihe 6')
-plt.xlabel(r'Winkeländerung $\delta\theta$')
+plt.xlabel(r'Winkeländerung $\delta\theta /$rad')
 plt.ylabel(r'Anzahl der Maxima $M$')
 plt.legend(loc='best')
 plt.grid()
 plt.savefig('Plaettchenplot.pdf')
 plt.clf()
 aes= unp.uarray(noms(aes),stds(aes))
-brechisp = 1/(1-(lambdvac*aes /20*d))
+brechisp = 1/(1-(lambdvac*aes /(2*(20*np.pi/360)*d)))
 print('Plaettchen Mittelbrech:', np.mean(brechisp))
 np.savetxt('plaettab.txt',np.column_stack([noms(aes),stds(aes),noms(brechisp),stds(brechisp)]), delimiter=' & ',newline= r'\\'+'\n' )
 ### Luft 
@@ -97,10 +97,10 @@ plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('Luftplot.pdf')
 np.savetxt('lufttab.txt',np.column_stack([noms(bpams),stds(bpams)]), delimiter=' & ',newline= r'\\'+'\n' )
 print(np.mean(bpams))
-print((np.mean(bpams)*T*1013*10**2/(15+const.zero_Celsius)) + 1)
+print(((np.mean(bpams)*T*1013*10**2/(15+const.zero_Celsius)) + 1)**0.5)
 print('litW:',(27663.8*10**(-8) +1))
-print('relf Lit Luft:', relf((27663.8*10**(-8) +1),(np.mean(bpams)*T*1013*10**2/(15+const.zero_Celsius)) + 1))
-print('relf Lit Plaettchen:', relf(1.5,np.mean(brechisp)))
+print('relf Lit Luft:', relf((27663.8*10**(-8) ),np.sqrt(noms((np.mean(bpams)*T*1013*10**2/(15+const.zero_Celsius)) + 1)) -1))
+print('relf Lit Plaettchen:', relf(.5,np.mean(brechisp)-1))
 
 
 
